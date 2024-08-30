1ab32bfdb011ddbffd945b957cbb72b21119aff2
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2024-08-29 22:29:15 -0600

    Using env variables to select groups of topics to create

--- kafka/kafka_init.sh
+++ kafka/kafka_init.sh
@@ -1,55 +1,17 @@
 #!/bin/sh
 
-#
-# Create or update topics
-#
-# Usage: 
-#   ./kafka_init.sh [app1] [app2] ...
-#
-# Arguments:
-#   List of application names that can include the following:
-#     - jpo-ode
-#     - jpo-geojsonconverter
-#     - jpo-conflictmonitor
-#     - jpo-deduplicator
-#
-
-echo "APPS=$@"
-
-JPO_ODE=false
-JPO_GEOJSONCONVERTER=false
-JPO_CONFLICTMONITOR=false
-JPO_DEDUPLICATOR=false
-for app in "$@"; do
-    if [ "$app" = "jpo-ode" ]; then
-        JPO_ODE=true
-    elif [ "$app" = "jpo-geojsonconverter" ]; then
-        JPO_GEOJSONCONVERTER=true
-    elif [ "$app" = "jpo-conflictmonitor" ]; then
-        JPO_CONFLICTMONITOR=true
-    elif [ "$app" = "jpo-deduplicator" ]; then
-        JPO_DEDUPLICATOR=true
-    else
-        echo "Unknown application: $app"
-        exit 1
-    fi
-done
-
-echo "JPO_ODE=$JPO_ODE"
-echo "JPO_GEOJSONCONVERTER=$JPO_GEOJSONCONVERTER"
-echo "JPO_CONFLICTMONITOR=$JPO_CONFLICTMONITOR"
-echo "JPO_DEDUPLICATOR=$JPO_DEDUPLICATOR"
+echo "KAFKA_TOPIC_CREATE_ODE=$KAFKA_TOPIC_CREATE_ODE"
+echo "KAFKA_TOPIC_CREATE_GEOJSONCONVERTER=$KAFKA_TOPIC_CREATE_GEOJSONCONVERTER"
+echo "KAFKA_TOPIC_CREATE_CONFLICTMONITOR=$KAFKA_TOPIC_CREATE_CONFLICTMONITOR"
+echo "KAFKA_TOPIC_CREATE_DEDUPLICATOR=$KAFKA_TOPIC_CREATE_DEDUPLICATOR"
 
 # Validate and log the filled-in template
-# Pass environment variables to jikkou. 
-# Note using this approach instead of jikkou --selector option because the latter
-# causes an error status to be returned for topics that already exit.
-JPO_ODE=${JPO_ODE} JPO_GEOJSONCONVERTER=${JPO_GEOJSONCONVERTER} JPO_CONFLICTMONITOR=${JPO_CONFLICTMONITOR} JPO_DEDUPLICATOR=${JPO_DEDUPLICATOR} ./jikkou validate \
+./jikkou validate \
      --files kafka-topics-template.jinja \
      --values-files kafka-topics-values.yaml 
 
 # Create or update topics
-JPO_ODE=${JPO_ODE} JPO_GEOJSONCONVERTER=${JPO_GEOJSONCONVERTER} JPO_CONFLICTMONITOR=${JPO_CONFLICTMONITOR} JPO_DEDUPLICATOR=${JPO_DEDUPLICATOR} ./jikkou apply \
+./jikkou apply \
     --files kafka-topics-template.jinja \
     --values-files kafka-topics-values.yaml 
 
